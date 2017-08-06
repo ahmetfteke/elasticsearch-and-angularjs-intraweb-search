b457537f6c5d6ee437621c2ca43f46a7b0b6e311
Ahmet Furkan Teke <ateke1@binghamton.edu>
2017-08-05 20:45:12 -0400

    Repeating Content for All Items in a List

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,21 @@
  */
 angular.module('searchEngineApp')
   .controller('MainCtrl', function ($scope) {
-    var movie = {
+    var movies = [{
       title: 'Brave Hearth',
       image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BODg4NzA0MTktOGU5ZS00ODlkLWE3ZmQtYjAzNjNmM2E3NmEzL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
       description: 'When his secret bride is executed for assaulting an English soldier who tried to rape her, Sir William Wallace begins a revolt against King Edward I of England.'
-    };
-    $scope.movie = movie;
+    },
+    {
+      title: 'Pulp Fiction',
+      image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      description: 'The lives of two mob hit men, a boxer, a gangsters wife, and a pair of diner bandits intertwine in four tales of violence and redemption.'
+    },
+    {
+      title: 'Fight Club',
+      image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BZGY5Y2RjMmItNDg5Yy00NjUwLThjMTEtNDc2OGUzNTBiYmM1XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      description: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.'
+    }
+    ];
+    $scope.movies = movies;
   });
